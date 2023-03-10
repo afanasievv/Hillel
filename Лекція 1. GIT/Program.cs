b12e6610d853aa83125057f4c45a375c37de0b45
unknown <af.vic97@gmail.com>
2023-03-10 17:10:47 +0200

    Add a project

---
+++ Лекція 1. GIT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            for (var i = 0; i < 100; i++)
+                sum += i;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
