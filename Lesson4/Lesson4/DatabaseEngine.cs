207593022536f8dc2b9b4106bc9215afb5313b5b
Viktor <af.vic97@gmail.com>
2023-03-28 16:48:13 +0300

    Add a Lesson4 folder

---
+++ Lesson4/Lesson4/DatabaseEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class DatabaseEngine
+    {
+        public void  WriteValue<T>(T value, string password)
+        {
+        var databaseValue= new DatabaseValue<T>();
+                       
+
+        }
+        public class DatabaseValue<T>
+        {   public T Value { get; set; }
+
+            public string TableName { get; set; }
+        }
+    }
+}
---
+++ Lesson4/Lesson4/PasswordGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    public class PasswordGenerator
+    {
+        public char[] allowedSymbols = new char[]{ };
+        public string Generate(char[] array)
+        {
+            return array[0].ToString();
+
+        }
+    }
+}
