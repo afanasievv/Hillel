e14d4678c89f6fc47a707512888fbefdc521e674
Viktor Afanasiev <af.vic97@gmail.com>
2023-05-15 10:18:33 +0300

    Add Lesson15

---
+++ Lesson14/MSSQLforCS/BestResaltGroupFunction.sql
@@ -0,0 +1,8 @@
+﻿--create function BestStudent()
+--returns table as
+--return 
+
+select Student_Name,Student_LastName, max(Student_Avg_mark) as 'Best group resalt'from Students
+group by Student_IdGroup
+--select * from Students
+--select * from BestStudent()
