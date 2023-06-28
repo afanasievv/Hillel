42474ff93ee4382967aa4247600c1c219c677f6d
Viktor Afanasiev <af.vic97@gmail.com>
2023-06-28 13:27:03 +0300

    Add Lesson24

---
+++ Lesson24/Lesson24/Controllers/GroupController.cs
@@ -0,0 +1,54 @@
+﻿using Lesson24;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GroupController : ControllerBase
+{
+    private static List<StudentGroup> cachedGroups = new List<StudentGroup>();
+    
+
+    [HttpPost]
+    public IActionResult Post([FromBody] StudentGroup group)
+    {
+        
+        if (!ValidateGroup(group))
+        {
+            return BadRequest("Некоректні дані групи");
+        }
+
+        
+        if (!IsGroupInCache(group))
+        {           
+            cachedGroups.Add(group);
+        }
+        return Ok(cachedGroups);
+    }
+
+    private bool ValidateGroup(StudentGroup group)
+    {
+        if (string.IsNullOrEmpty(group.Name) || group.Name.Length < 2 || group.Name.Length > 5)
+        {
+            return false;
+        }
+
+        if (group.Course <= 0 || group.Course >= 7)
+        {
+            return false;
+        }
+
+        Regex regex = new Regex(@"^[A-Za-z]{3}-\d{2}$");
+        if (!regex.IsMatch(group.Name))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsGroupInCache(StudentGroup group)
+    {
+        return cachedGroups.Any(g => g.Name == group.Name && g.Course == group.Course);
+    }
+}
---
+++ Lesson24/Lesson24/StudentGroup.cs
@@ -0,0 +1,8 @@
+﻿namespace Lesson24
+{
+    public class StudentGroup
+    {
+        public string Name {get;set;}
+        public int Course { get; set; }
+    }
+}
